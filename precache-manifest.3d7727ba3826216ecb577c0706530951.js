d766430c34037e041aae97c7fceb57e36c0e6c48
Alberta, Tirza <tirza.alberta@nyu.edu>
2020-05-24 18:47:26 +0700

    Updates

---
+++ precache-manifest.3d7727ba3826216ecb577c0706530951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ef1184ba35813977446386caa86e6a",
+    "url": "/goodreads-but/index.html"
+  },
+  {
+    "revision": "115b043e06ff3acd313c",
+    "url": "/goodreads-but/static/css/main.dafeccd9.chunk.css"
+  },
+  {
+    "revision": "232e12625ef1d9195509",
+    "url": "/goodreads-but/static/js/2.66d8040c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goodreads-but/static/js/2.66d8040c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115b043e06ff3acd313c",
+    "url": "/goodreads-but/static/js/main.f965c946.chunk.js"
+  },
+  {
+    "revision": "cfb214e61e20eab26f05",
+    "url": "/goodreads-but/static/js/runtime-main.4c0d47c8.js"
+  }
+]);
